3b3c7d0852ee152a6d89c097bbbd433a41d500f5
F4cel3ss <johnanthonysolana7@gmail.com>
2025-06-25 10:44:28 +0800

    optimizing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import profile from "../assets/profile.webp";
 import { FcDownload } from "react-icons/fc";
+
 type TypewriterProps = {
   words: string[];
   speed?: number;
 };
 
-function Typewriter({ words, speed = 150}: TypewriterProps) {
-  const [index, setIndex] = useState(0);      // Current word index
-  const [subIndex, setSubIndex] = useState(0); // Characters typed
+function Typewriter({ words, speed = 150 }: TypewriterProps) {
+  const [index, setIndex] = useState(0);
+  const [subIndex, setSubIndex] = useState(0);
   const [reverse, setReverse] = useState(false);
 
   useEffect(() => {
@@ -42,17 +43,19 @@ function Typewriter({ words, speed = 150}: TypewriterProps) {
 
 function Header() {
   return (
-    <header className="w-full flex   gap-4 flex-row ">
-      <div className="w-[100%] flex justify-center xl:w-[45%]">
+    <header className="w-full flex gap-4 flex-row">
+      <div className="w-full flex justify-center xl:w-[45%]">
         <img
           src={profile}
-        alt="John Anthony Solana's Profile Picture"
-          className="w-full rounded-2xl  max-w-[300px] max-h-[300px]  "
+          alt="John Anthony Solana's Profile Picture"
+          className="w-full max-w-[300px] max-h-[300px] object-cover rounded-2xl"
+          loading="eager" 
         />
       </div>
-      <div className="w-full  flex flex-col justify-center items-start   relative  ">
-        
-<div className="tooltip tooltip-bottom  absolute top-0 right-0">
+
+      {/* Text Section */}
+      <div className="w-full flex flex-col justify-center items-start relative">
+      <div className="tooltip tooltip-bottom  absolute top-0 right-0">
   <div className="tooltip-content">
     <div className="animate-bounce text-orange-400 -rotate-10 text-xl font-black">yehey!</div>
   </div>
@@ -62,19 +65,18 @@ function Header() {
   </a>
 </div>
 
-
-
-       
         <h1 className="text-neutral font-black font-primary text-2xl mt-2 min-[450px]:text-4xl">
           Jars.Dev
         </h1>
-        <p className="text-xs mt-2 font-medium tracking-tight s:text-base ">
-          I'm a <Typewriter words={[" Web Developer"]} />
+        {/* Role with Typewriter */}
+        <p className="text-xs mt-2 font-medium tracking-tight s:text-base">
+          I'm a <Typewriter words={["Web Developer"]} />
         </p>
-         <div className="flex items-center gap-2 mt-2">
+
+        <div className="flex items-center gap-2 mt-2">
           <div aria-label="status" className="status status-primary animate-pulse" />
           <p className="text-xs">Available To Work</p>
-        </div>  
+        </div>
       </div>
     </header>
   );
