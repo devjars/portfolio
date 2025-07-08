6ae4ba32215bd354dcc072ccbf19204a39ab8753
F4cel3ss <johnanthonysolana7@gmail.com>
2025-07-08 20:26:26 +0800

    reduce gap in stastus component

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -4,7 +4,7 @@ import { GoPlus } from "react-icons/go";
 import { FaUserTie } from "react-icons/fa";
 function Status() {
   return (
-    <div className="w-full h-full grid grid-cols-2 gap-4 ">
+    <div className="w-full h-full grid grid-cols-2 gap-2 ">
          <div className="bg-neutral/10 p-4 rounded-xl flex flex-col items-center justify-center gap-4 shadow-md border border-neutral/10 ">
             <h2 className="flex items-center  text-5xl font-black">0{project}<GoPlus className="text-2xl"/></h2>
             <p className="w-full flex justify-center items-center gap-1  py-1 bg-neutral/10 rounded-full font-medium tracking-tight text-sm s:text-base"> <IoExtensionPuzzle/>Projects</p>
